8df0073023fb0f318b93d7574db69c6509a1e15e
Curry4182 <kangbk4182@naver.com>
2023-07-22 03:28:36 +0900

    feat: jdbc를 이용하여  컴퓨터부품(ComputerPart) 수정, 전체 조회  기능 추가

--- src/main/java/kr/co/programmers/partsmarket/repository/ComputerComputerPartJdbcRepository.java
+++ src/main/java/kr/co/programmers/partsmarket/repository/ComputerComputerPartJdbcRepository.java
@@ -1,14 +1,20 @@
 package kr.co.programmers.partsmarket.repository;
 
+import java.nio.ByteBuffer;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import kr.co.programmers.partsmarket.model.ComputerPart;
+import kr.co.programmers.partsmarket.model.ComputerPartCategory;
 
 public class ComputerComputerPartJdbcRepository implements ComputerPartRepository {
 
@@ -20,7 +26,7 @@ public class ComputerComputerPartJdbcRepository implements ComputerPartRepositor
 
 	@Override
 	public List<ComputerPart> findAll() {
-		return null;
+		return namedParameterJdbcTemplate.query("select * from computer_parts", computerPartRowMapper);
 	}
 
 	@Override
@@ -30,12 +36,25 @@ public class ComputerComputerPartJdbcRepository implements ComputerPartRepositor
 			+ "VALUES (UNHEX(REPLACE(:partId, '-', '')), :partName, :partCategory, :partPrice, partDescription, partCreatedAt"
 		, toParamMap(computerPart));
 
+		if (insertedPartNum != 1) {
+			throw new RuntimeException("Nothing was inserted");
+		}
+
 		return computerPart;
 	}
 
 	@Override
 	public ComputerPart update(ComputerPart computerPart) {
-		return null;
+		int updatedComputerPartNum = namedParameterJdbcTemplate.update(
+			"UPDATE computer_parts SET part_id = :partId, part_name = :partName, part_category = :partCategory, part_price = :partPrice, part_description = :partDescription, part_created_at = :partCreatedAt "
+			+ " where part_id = :partId", toParamMap(computerPart)
+		);
+
+		if(updatedComputerPartNum < 1) {
+			throw new RuntimeException("Nothing was updated!");
+		}
+
+		return computerPart;
 	}
 
 	@Override
@@ -48,6 +67,24 @@ public class ComputerComputerPartJdbcRepository implements ComputerPartRepositor
 
 	}
 
+	private RowMapper<ComputerPart> computerPartRowMapper = (ResultSet resultSet, int i) ->
+	{
+		var partId = toUUID(resultSet.getBytes("part_id"));
+		var partName = resultSet.getString("part_name");
+		var partCategory = ComputerPartCategory.valueOf(resultSet.getString("part_category"));
+		var partPrice = resultSet.getInt("part_price");
+		var partDescription = resultSet.getString("part_description");
+		var partCreatedAt = toLocalDateTime(resultSet.getTimestamp("part_created_at"));
+		return new ComputerPart(
+			partId,
+			partName,
+			partCategory,
+			partPrice,
+			partDescription,
+			partCreatedAt
+		);
+	};
+
 	private Map<String, Object> toParamMap(ComputerPart computerPart) {
 		Map<String, Object> paramMap = new HashMap<>();
 		paramMap.put("partId", computerPart.getPartId().toString());
@@ -57,5 +94,14 @@ public class ComputerComputerPartJdbcRepository implements ComputerPartRepositor
 		paramMap.put("partDescription", computerPart.getPartDescription());
 		paramMap.put("partCreatedAt", computerPart.getPartCreatedAt());
 		return paramMap;
+	}
+
+	static public UUID toUUID(byte[] bytes) {
+		var byteBuffer = ByteBuffer.wrap(bytes);
+		return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+	}
+
+	static public LocalDateTime toLocalDateTime(Timestamp timestamp) {
+		return timestamp != null ? timestamp.toLocalDateTime() : null;
 	}
 }
