b07d66244d59c506dead6a5363fc1a3582391943
Curry4182 <kangbk4182@naver.com>
2023-07-22 01:32:09 +0900

    init

---
+++ src/main/java/kr/co/programmers/partsmarket/PartsMarketApplication.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.partsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartsMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/programmers/partsmarket/PartsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.programmers.partsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
