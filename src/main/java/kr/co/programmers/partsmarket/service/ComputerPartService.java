f6a722359ef1578f0059bf2cbdf32cdabadd3f54
Curry4182 <kangbk4182@naver.com>
2023-07-22 16:28:45 +0900

    feat: computer part를 생성, 조회 하는 service 기능 추가

---
+++ src/main/java/kr/co/programmers/partsmarket/service/ComputerPartService.java
@@ -0,0 +1,12 @@
+package kr.co.programmers.partsmarket.service;
+
+import java.util.List;
+
+import kr.co.programmers.partsmarket.model.ComputerPart;
+import kr.co.programmers.partsmarket.model.ComputerPartCategory;
+
+public interface ComputerPartService {
+	List<ComputerPart> getAllComputerParts();
+
+	ComputerPart createComputerPart(String computerPartName, ComputerPartCategory computerPartCategory, int price, String description);
+}
---
+++ src/main/java/kr/co/programmers/partsmarket/service/DefaultComputerPartService.java
@@ -0,0 +1,41 @@
+package kr.co.programmers.partsmarket.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.programmers.partsmarket.model.ComputerPart;
+import kr.co.programmers.partsmarket.model.ComputerPartCategory;
+import kr.co.programmers.partsmarket.repository.ComputerPartRepository;
+
+import java.util.UUID;
+@Service
+public class DefaultComputerPartService implements ComputerPartService{
+
+	private final ComputerPartRepository computerPartRepository;
+
+	public DefaultComputerPartService(ComputerPartRepository computerPartRepository) {
+		this.computerPartRepository = computerPartRepository;
+	}
+
+	@Override
+	public List<ComputerPart> getAllComputerParts() {
+		return computerPartRepository.findAll();
+	}
+
+	@Override
+	public ComputerPart createComputerPart(String computerPartName, ComputerPartCategory computerPartCategory,
+		int price, String description) {
+		ComputerPart newComputerPart = new ComputerPart(
+			UUID.randomUUID(),
+			computerPartName,
+			computerPartCategory,
+			price,
+			description,
+			LocalDateTime.now()
+		);
+		computerPartRepository.insert(newComputerPart);
+		return newComputerPart;
+	}
+}
